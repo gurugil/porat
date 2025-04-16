8d186218d6854ca684a6256290f3bba942b8a349
gurugil <gurugil@gmail.com>
2025-04-16 19:09:23 +0300

    Add files via upload

---
+++ porat_site_ready/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <main className="p-10 text-center">
+      <h1 className="text-4xl font-bold mb-4">דניס הישרדות פורת</h1>
+      <p className="text-lg">אומנות לחימה שהופכת אותך ללוחם אמיתי</p>
+    </main>
+  );
+}
